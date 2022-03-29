d1c54e96751a8656c0c99d7a5b6bb4acc6ef07b6
Taehun Jeong <hoonystory98@gmail.com>
2022-03-30 07:18:07 +0900

    Create 2738.cpp

---
+++ 2738.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+const int MAX = 105;
+
+int main() {
+	int n, m;
+	int a[MAX][MAX], b[MAX][MAX];
+
+	cin >> n >> m;
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cin >> a[i][j];
+		}
+	}
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cin >> b[i][j];
+		}
+	}
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cout << a[i][j] + b[i][j] << " ";
+		}
+		cout << "\n";
+	}
+
+	return 0;
+}
