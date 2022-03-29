f46ce8af545553fa92ee3e75604685bbbc748363
Taehun Jeong <hoonystory98@gmail.com>
2022-03-30 07:18:28 +0900

    Create 2739.cpp

---
+++ 2739.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a,i;
+    cin >> a;
+    for(i=1; i<10; i++)
+        cout << a << " * " << i << " = " << a*i << "\n";
+    return 0;
+}
