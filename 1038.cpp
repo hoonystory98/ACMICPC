1843864717caa7bb7d010f4c2ecf25497998974e
Taehun Jeong <hoonystory98@gmail.com>
2022-07-21 12:59:16 +0900

    Create 1038.cpp

---
+++ 1038.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool check[10] = { false, };
+vector<pair<int, string>> v;
+
+void backtr(int maxsize, int vsize, int start) {
+	if (vsize == maxsize) {
+		string s;
+		for (int i = 9; i >= 0; i--) {
+			if (check[i]) {
+				s += (char)('0' + i);
+			}
+		}
+		v.push_back({ vsize, s });
+	}
+	else {
+		for (int i = start; i < 10; i++) {
+			if (check[i] == false) {
+				check[i] = true;
+				backtr(maxsize, vsize + 1, i + 1);
+				check[i] = false;
+			}
+		}
+	}
+}
+
+int main() {
+	int n;
+
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	for (int i = 1; i <= 10; i++) {
+		backtr(i, 0, 0);
+	}
+	sort(v.begin(), v.end());
+
+	cin >> n;
+	if (n >= v.size()) {
+		cout << -1;
+	}
+	else {
+		cout << v.at(n).second;
+	}
+
+	return 0;
+}
